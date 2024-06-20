95c4a0738de92060276fbc5f7ec1676202c20856
TijanAyo <ayomidetijani6@gmail.com>
2024-06-20 21:14:24 +0100

    Added a repository layer to handle service logic

--- src/repositories/auth.repository.ts
+++ src/repositories/auth.repository.ts
@@ -1,4 +1,40 @@
 import { injectable } from "tsyringe";
+import User from "../models/user.model";
+import { badRequestException } from "../helpers";
 
 @injectable()
-export class AuthRepository {}
+export class AuthRepository {
+  public async lookUp(email: string) {
+    try {
+      return await User.findOne({ emailAddress: email });
+    } catch (err: any) {
+      console.error("Error looking up user by email:", err);
+      throw err;
+    }
+  }
+
+  public async createUser(
+    email: string,
+    firstName: string,
+    lastName: string,
+    phoneNumber: string,
+    password: string,
+  ) {
+    try {
+      const user = await User.create({
+        emailAddress: email,
+        firstName,
+        lastName,
+        phoneNumber,
+        password,
+      });
+
+      if (!user) throw new badRequestException("User could not be created");
+
+      return user;
+    } catch (err: any) {
+      console.error("Error creating user:", err);
+      throw err;
+    }
+  }
+}
