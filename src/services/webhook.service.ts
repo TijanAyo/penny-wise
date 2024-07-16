cb16188c80b8c0cefaf0a5a03d62f6a5588b6a5e
TijanAyo <ayomidetijani6@gmail.com>
2024-07-16 23:44:28 +0100

    Adding more console.logs

--- src/services/webhook.service.ts
+++ src/services/webhook.service.ts
@@ -27,64 +27,6 @@ export class WebHookService {
     headers: this.FLUTTERWAVE_HEADER_CONFIG,
   });
 
-  private async chargeSuccessEvent(
-    payloadId: number,
-    customerMail: string,
-    payloadAmount: number,
-    res: Response,
-  ) {
-    try {
-      const { data } = await this.verifyTransactionEvent(payloadId, res);
-
-      const user = await this._userRepository.findByEmail(customerMail);
-      if (!user) {
-        console.log("chargeSuccessEventError: User not found");
-        res.status(401).send("User not found").end();
-      }
-
-      const updateWallet = await this._walletRepository.incrementBalance(
-        user._id,
-        payloadAmount,
-      );
-      if (!updateWallet) {
-        console.log("An error occurred while updating wallet");
-        res.status(401).send("Balance could not be updated").end();
-      }
-
-      const transactionData = {
-        from: `Virtual account transfer`,
-        recipient_name: data.data.meta.originatorname as string,
-        recipient_bank: data.data.meta.bankname as string,
-        amount_credited: String(payloadAmount),
-        type: TransactionType.FUNDING,
-        status: TransactionStatus.SUCCESSFUL,
-        reference: generateTransactionReference(),
-        description: `Transfer to wallet from bank account ${data.data.meta.originatoraccountnumber} - ${data.data.meta.originatorname}`,
-      };
-      const { _id } = await this._walletRepository.getWalletInfo(user._id);
-
-      const transaction = await this._transactionService.createTransaction(
-        transactionData,
-        _id,
-      );
-      if (!transaction) {
-        console.log("An issue occured while trying to create transaction");
-        res.status(401).send("Transaction could not be updated").end();
-      }
-
-      // Update wallet transactions
-      await this._walletRepository.updateTransactionsInWallet(
-        _id,
-        transaction._id,
-      );
-
-      console.log("Transaction successfully processed");
-    } catch (error: any) {
-      console.error("chargeSuccessEventError:", error);
-      res.status(500).send("Internal server error");
-    }
-  }
-
   private async verifyTransactionEvent(payloadId: number, res: Response) {
     try {
       const response = await this.FLUTTERWAVE_CLIENT.get(
@@ -97,10 +39,75 @@ export class WebHookService {
         res.status(401).send("Transaction could not be verified").end();
       }
 
+      console.log("Got here 1");
       return { data: response.data };
     } catch (error: any) {
       console.error("verifyTransactionEventError:", error);
       throw error;
+    }
+  }
+
+  private async chargeSuccessEvent(
+    payloadId: number,
+    customerMail: string,
+    payloadAmount: number,
+    res: Response,
+  ) {
+    try {
+      const { data } = await this.verifyTransactionEvent(payloadId, res);
+
+      console.log("Got here 2");
+      const user = await this._userRepository.findByEmail(customerMail);
+      if (!user) {
+        console.log("chargeSuccessEventError: User not found");
+        res.status(401).send("User not found").end();
+      }
+
+      console.log("got here 3");
+      const updateWallet = await this._walletRepository.incrementBalance(
+        user._id,
+        payloadAmount,
+      );
+      if (!updateWallet) {
+        console.log("An error occurred while updating wallet");
+        res.status(401).send("Balance could not be updated").end();
+      }
+
+      console.log("got here 4");
+      const transactionData = {
+        from: `Virtual account transfer`,
+        recipient_name: data.data.meta.originatorname as string,
+        recipient_bank: data.data.meta.bankname as string,
+        amount_credited: String(payloadAmount),
+        type: TransactionType.FUNDING,
+        status: TransactionStatus.SUCCESSFUL,
+        reference: generateTransactionReference(),
+        description: `Transfer to wallet from bank account ${data.data.meta.originatoraccountnumber} - ${data.data.meta.originatorname}`,
+      };
+      console.log("got here 5");
+      const { _id } = await this._walletRepository.getWalletInfo(user._id);
+
+      console.log("got here 6");
+      const transaction = await this._transactionService.createTransaction(
+        transactionData,
+        _id,
+      );
+      if (!transaction) {
+        console.log("An issue occured while trying to create transaction");
+        res.status(401).send("Transaction could not be updated").end();
+      }
+      console.log("got here 7");
+
+      // Update wallet transactions
+      await this._walletRepository.updateTransactionsInWallet(
+        _id,
+        transaction._id,
+      );
+
+      console.log("Transaction successfully processed");
+    } catch (error: any) {
+      console.error("chargeSuccessEventError:", error);
+      res.status(500).send("Internal server error");
     }
   }
 
